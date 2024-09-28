6e96c678ac27181754228e93aa534fc1329df777
maranix <ramanverma4183@gmail.com>
2024-09-28 13:10:11 +0530

    feat(pkg/cli): add actionHandler

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -1,6 +1,8 @@
 package cli
 
 import (
+	"fmt"
+
 	"github.com/urfave/cli/v2"
 )
 
@@ -36,9 +38,26 @@ func (a *App) RegisterConfig(config *Config) {
 }
 
 func (a *App) Run(args []string) error {
+	a.cliApp.Action = actionHandler
+
 	if err := a.cliApp.Run(args); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func actionHandler(cCtx *cli.Context) error {
+	argsLen := cCtx.Args().Len()
+
+	// TODO: Implement TUI
+	if argsLen < 1 {
+		return fmt.Errorf("Expected to execute TUI, functionality is not yet implemented!")
+	}
+
+	// TODO: Implement template downloading functionality
+	//
+	// 1. If only 1 arg is provided then download it simply as .gitignore
+	// 2. If more than 1 args are provided then download as template names
+	return fmt.Errorf("Expected to download .gitignore, functionality is not yet implemented!")
 }
